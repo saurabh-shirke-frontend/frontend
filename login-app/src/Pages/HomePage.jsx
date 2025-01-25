b01336956439a862cfaad719597740024dbd64f4
Saurabh Shirke <saurabh.shirke@coherent.com.hk>
2025-01-25 16:24:52 +0530

    Login app changes

---
+++ login-app/src/Pages/HomePage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../Components/Button";
+import './HomePage.css';
+import '../Components/Button.css';
+import videoTrailer from '../Components/videos/video1.mp4';
+
+export const HomePage = () => {
+    const payload = JSON.parse(localStorage.getItem('userData'));
+
+    console.log(payload);
+    const rollback = useNavigate();
+    const handleSubmit = () => {
+        rollback('/');
+    }
+    return (
+      <div className='base-container'>
+        {
+          payload && (
+            <div className="profile-image-container">
+              {payload.picture && <img src={payload.picture} className="user-img" alt="User"/>}
+            </div>
+          )
+        }
+        <video src={videoTrailer} autoPlay loop muted />
+        <h1>Adventure Awaits</h1>
+        <p>"{payload.first} {payload.last}"</p>
+        <p>What are your waiting for?</p>
+        <div className='base-btns'>
+          <Button
+            className="btn"
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'>
+            GET STARTED
+          </Button>
+          <Button
+            className="btn"
+            buttonStyle='btn--primary'
+            buttonSize='btn--large'>
+            Watch Trailer
+          </Button>
+        </div>
+        <button className="custom-button" onClick={handleSubmit}>Log Out</button>
+      </div>
+    )
+}
